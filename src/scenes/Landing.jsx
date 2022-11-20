3312ea228e47bbc01358be7ec96eadb8bcbe0317
mbrndt <mareike.brandt@hotmail.de>
2022-11-20 19:54:08 +0900

    feat: finished landing page

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,6 +1,7 @@
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Landing = ({setSelectedPage}) => {
     const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
@@ -60,6 +61,39 @@ const Landing = ({setSelectedPage}) => {
               viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
             </p>
           </motion.div>
+
+          {/* call to actions */}
+            <motion.div
+            className="flex mt-5 justify-center md:justify-start"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.2, duration: 0.5 }}
+                variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+                }}
+            >
+                <AnchorLink href="#contact" className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue hover:text-white transition duration-500" onClick={() => setSelectedPage("contact")}>Contact Me</AnchorLink>
+                <AnchorLink href="#contact" className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5" onClick={() => setSelectedPage("contact")}>
+                    <div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center font-playfair px-10">
+                    Let's Talk
+                    </div></AnchorLink>
+
+                </motion.div>
+
+                <motion.div
+                className="flex mt-5 justify-center md:justify-start"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.4, duration: 0.5 }}
+                variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+                }}>
+                    <SocialMediaIcons />
+                </motion.div>
                      
             </div>
         </section>
